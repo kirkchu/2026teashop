7555ea212c54cc0000bee89bd8a5946f1a720590
kirkchu <kirkchu@gmail.com>
2026-07-24 14:53:54 +0800

    Connect deployed Google Sheets order API

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 部署 Apps Script 後，請將 Web app URL 填入此處。
-window.ORDER_API_URL = "";
+window.ORDER_API_URL = "https://script.google.com/macros/s/AKfycbyCytSvZbWrCNydbsz_A-Ctyy6ws1BORMl4pXC4omU169eqwPM6ijep-Pmo0To11mkN/exec";
